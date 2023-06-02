c4a7e4f3546703a49ebb3b2ca2c1cd8f97265b0d
Yahyap <yahyap20@gmail.com>
2023-06-02 15:44:48 +0700

    Rename from authRoute to authRouter

---
+++ routes/authRouter.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const jwt = require("jsonwebtoken");
+const authController = require("../controllers/authController");
+const jwtAuth = require("../middleware/jwtAuth");
+
+router.use((req, res, next) => {
+  console.log("Time: ", Date.now());
+  next();
+});
+
+router.post("/register", authController.signup);
+router.post("/login", authController.signin);
+router.get("/protected", jwtAuth(), authController.protected);
+
+module.exports = router;
