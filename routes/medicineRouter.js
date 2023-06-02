2e25965f91b05c5f47abe61a2cc7df1ea1f6a6cd
Yahyap <yahyap20@gmail.com>
2023-06-02 15:44:09 +0700

    Just add jwt

--- routes/medicineRouter.js
+++ routes/medicineRouter.js
@@ -3,8 +3,13 @@ const router = express.Router();
 const medicineController = require("../controllers/medicineController");
 const jwtAuth = require("../middleware/jwtAuth");
 
-router.get("/getmed", medicineController.getMed);
-router.get("/specificmed/:id_artikel", medicineController.specificmed);
-router.get("/home", medicineController.home);
+router.use((req, res, next) => {
+  console.log("Time: ", Date.now());
+  next();
+});
+
+router.get("/specificmed/:id_artikel", jwtAuth(), medicineController.specificmed);
+router.get("/searchmed/:search", jwtAuth(), medicineController.searchmed);
+router.get("/home", jwtAuth(), medicineController.home);
 
 module.exports = router;
