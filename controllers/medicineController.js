750a7813b872561b85ecc1728edfded9b8328e92
Yahyap <yahyap20@gmail.com>
2023-06-02 15:47:19 +0700

    remove unnecessary line

--- controllers/medicineController.js
+++ controllers/medicineController.js
@@ -1,68 +1,88 @@
 const connection = require("../mysql/connect");
 
-exports.getMed = async (req, res) => {
-  console.log("Cari .....");
-  let { tag } = req.body;
-
-  let db = `
-SELECT * FROM tabel_artikel WHERE artikel LIKE "%${tag}%"
-  `;
-
-  connection.query(db, function (err, data) {
-    if (data.length <= 0) {
-      return res.status(404).json({
-        status: "Failed",
-        requestAt: new Date().toISOString(),
-        message: "Not Found",
-      });
-    }
-    return res.status(201).json({
-      status: "Success",
-      artikel: data,
-      requestAt: new Date().toISOString(),
-    });
-  });
-};
-
 exports.specificmed = async (req, res) => {
-  const id_artikel = req.params.id_artikel;
-  let db = `
-  SELECT * FROM tabel_artikel WHERE id LIKE "%${id_artikel}%"
+  try {
+    const id_artikel = req.params.id_artikel;
+    console.log(id_artikel);
+    let db = `
+  SELECT * FROM tabel_artikel WHERE id = ${id_artikel}
     `;
-  connection.query(db, function (err, data) {
-    if (data.length <= 0) {
-      return res.status(404).json({
-        status: "Failed",
+    connection.query(db, function (err, data) {
+      if (data.length <= 0) {
+        return res.status(404).json({
+          status: "Failed",
+          requestAt: new Date().toISOString(),
+          message: "Not Found",
+        });
+      }
+      return res.status(201).json({
+        status: "Success",
         requestAt: new Date().toISOString(),
-        message: "Not Found",
+        artikel: data,
       });
-    }
-    return res.status(201).json({
-      status: "Success",
-      artikel: data,
-      requestAt: new Date().toISOString(),
     });
-  });
+  } catch (err) {
+    return res.status(err.code).json({
+      status: "Failed",
+      message: err.message,
+    });
+  }
 };
 
 exports.home = async (req, res) => {
-  console.log("Home .....");
-  let db = `
-SELECT * FROM tabel_artikel LIMIT 5
-  `;
-
-  connection.query(db, function (err, data) {
-    if (data.length <= 0) {
-      return res.status(404).json({
-        status: "Failed",
+  try {
+    console.log("Home .....");
+    let db = `
+    SELECT tabel_artikel.id, tabel_artikel.nama_obat, tabel_artikel.deskripsi, tabel_artikel.foto_obat FROM tabel_artikel ORDER BY RAND() LIMIT 5
+      `;
+    connection.query(db, function (err, data) {
+      if (data.length <= 0) {
+        return res.status(404).json({
+          status: "Failed",
+          requestAt: new Date().toISOString(),
+          message: "Not Found",
+        });
+      }
+      return res.status(201).json({
+        status: "Success",
         requestAt: new Date().toISOString(),
-        message: "Not Found",
+        artikel: data,
       });
-    }
-    return res.status(201).json({
-      status: "Success",
-      artikel: data,
-      requestAt: new Date().toISOString(),
     });
-  });
+  } catch (err) {
+    return res.status(err.code).json({
+      status: "Failed",
+      message: err.message,
+    });
+  }
+};
+
+exports.searchmed = async (req, res) => {
+  try {
+    const searchmed = req.params.search;
+    let db = `
+    SELECT tabel_artikel.id, tabel_artikel.nama_obat, tabel_artikel.deskripsi, tabel_artikel.foto_obat FROM tabel_artikel WHERE nama_obat LIKE '%${searchmed}%' OR khasiat LIKE '%${searchmed}%' OR deskripsi LIKE '%${searchmed}%'
+      `;
+    console.log(searchmed);
+    connection.query(db, function (err, data) {
+      console.log(data);
+      if (data.length <= 0) {
+        return res.status(404).json({
+          status: "Failed",
+          requestAt: new Date().toISOString(),
+          message: "Not Found",
+        });
+      }
+      return res.status(201).json({
+        status: "Success",
+        requestAt: new Date().toISOString(),
+        artikel: data,
+      });
+    });
+  } catch (err) {
+    return res.status(err.code).json({
+      status: "Failed",
+      message: err.message,
+    });
+  }
 };
