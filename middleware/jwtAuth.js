1bb95f9e76b5513f5cc66d66a1632c7c9c2afb4e
Yahyap <yahyap20@gmail.com>
2023-06-02 15:46:14 +0700

    Fixing a Bug

--- middleware/jwtAuth.js
+++ middleware/jwtAuth.js
@@ -3,22 +3,29 @@ const jwt = require("jsonwebtoken");
 dotenv.config();
 
 const jwtAuth = () => {
-  return async (req, res, next) => {
-    const authorization = req.headers.authorization;
-    const token = authorization.split(" ")[1];
+  try {
+    return async (req, res, next) => {
+      const authorization = req.headers.authorization;
 
-    if (!token) {
-      return res.status(401).json({ error: "Unauthorized" });
-    }
-
-    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-      if (err) {
-        return res.status(401).json({ error: "Invalid token" });
+      if (!authorization) {
+        return res.status(401).json({ error: "Unauthorized" });
       }
-      req.jwt = decoded;
-      next();
+
+      const token = authorization.split(" ")[1];
+      jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).json({ error: "Invalid token" });
+        }
+        req.jwt = decoded;
+        next();
+      });
+    };
+  } catch (err) {
+    return res.status(err.code).json({
+      status: "Failed",
+      message: err.message,
     });
-  };
+  }
 };
 
 module.exports = jwtAuth;
