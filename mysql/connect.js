1f9f2879cb3869660151c213068184b9cd919a67
Yahyap <yahyap20@gmail.com>
2023-06-02 15:45:56 +0700

    change to poolconnection

--- mysql/connect.js
+++ mysql/connect.js
@@ -1,13 +1,13 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
   host: process.env.DB_HOSTNAME,
   database: process.env.DB_NAME,
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
 });
 
-connection.connect(function (error) {
+connection.getConnection(function (error) {
   if (error) {
     throw error;
   } else {
@@ -16,16 +16,3 @@ connection.connect(function (error) {
 });
 
 module.exports = connection;
-
-// const mysql = require("mysql");
-
-// const dbConfig = {
-//   host: "localhost",
-//   user: "root",
-//   password: "password",
-//   database: "mydatabase",
-// };
-
-// const connection = mysql.createPool(dbConfig);
-
-// module.exports = connection;
