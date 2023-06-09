ee1b3cd9e9cdfb6bb22ec0c5d64f54432f98fada
Yahyap <yahyap20@gmail.com>
2023-06-09 10:39:18 +0700

    Get detail Penyakit

---
+++ controllers/diseasesController.js
@@ -0,0 +1,30 @@
+const connection = require("../mysql/connect");
+
+exports.specificdis = async (req, res) => {
+  try {
+    const id_penyakit = req.params.id_penyakit;
+    console.log(id_penyakit);
+    let db = `
+      SELECT * FROM tabel_penyakit WHERE id_penyakit = ${id_penyakit}
+        `;
+    connection.query(db, function (err, data) {
+      if (data.length <= 0) {
+        return res.status(404).json({
+          status: "Failed",
+          requestAt: new Date().toISOString(),
+          message: "Not Found",
+        });
+      }
+      return res.status(201).json({
+        status: "Success",
+        requestAt: new Date().toISOString(),
+        artikel: data,
+      });
+    });
+  } catch (err) {
+    return res.status(err.code).json({
+      status: "Failed",
+      message: err.message,
+    });
+  }
+};
