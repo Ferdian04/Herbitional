a7e151addb4bfe8f902de9ef6f4785f1e7cca995
Ferdian Hafiz <89018562+Ferdian04@users.noreply.github.com>
2023-06-09 22:10:45 +0700

    ubah port pake os

--- flask_deploy.py
+++ flask_deploy.py
@@ -106,4 +106,4 @@ def index():
 
 # Run the Flask application
 if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
